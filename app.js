0b42aa6365d2b1862d32b657cc4d5574c92a4d19
kenyacaldwell <caldwellk44@gmail.com>
2023-12-22 16:57:27 -0600

    started JS

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
-// Players
-var yellowPlayer = "y"
-var redPlayer = "r"
-var player = yellowPlayer
+let playBoard;
+let player;
+let winner;
+
+const boardEL = document.getElementById("playboard")
+const playAgain = document.getElementById("play-again")
+playAgain.addEventListener("click", init)
+boardEL.addEventListener("click", handleClick)
+
+function init () { 
+    playBoard = [
+        [null,null,null,null,null,null],
+        [null,null,null,null,null,null],
+        [null,null,null,null,null,null],
+        [null,null,null,null,null,null],
+        [null,null,null,null,null,null],
+        [null,null,null,null,null,null],
+        [null,null,null,null,null,null],
+    ]
+    player = 1
+    winner = null
+    render()
+}
+init()
 
 
+function handleClick(evt) {
+    const row = parseInt(evt.target.id[1])
+    const col = playBoard[row].indexOf(null)
+    playBoard[row][col] = player
+    player *= -1
+    winner = checkWinner()
+    render()
+}
 
-var playBoard;
-var gameOver = false;
+function render(){
+    for(let i = 0; i < playBoard.length; i++) {
+        for(let j = 0; j < playBoard[i].length; j++) {
+            const cell = document.getElementById(`r${i}c${j}`)
+            if(playBoard[i][j] === 1) cell.style.backgroundColor = "red" 
+            if(playBoard[i][j] === -1) cell.style.backgroundColor = "yellow" 
+            if(playBoard[i][j] === null) cell.style.backgroundColor = "white" 
+        }
+    }
+}
 
+function checkWinner() {
 
-
-
-
-// Rows and colums
-var colums = 7;
-var rows = 6;
-
-// Game function
+}
